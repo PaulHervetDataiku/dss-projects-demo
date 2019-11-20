b2f884fa6a60d8d818b576d540f3a2c4215b6cf7
admin <admin>
2019-11-20 12:28:30 +0100

    Updated Lambda service config

--- lambda_services/python_prediction_service/endpoint-python_prediction_endpoint.py
+++ lambda_services/python_prediction_service/endpoint-python_prediction_endpoint.py
@@ -1,4 +1,3 @@
-
 from  dataiku.apinode.predict.predictor import ClassificationPredictor
 import pandas as pd
 import pickle
@@ -8,35 +7,21 @@ class MyPredictor(ClassificationPredictor):
     def __init__(self, data_folder):
         self.data_folder = data_folder
 
-    def predict(self, features_df):
-
-
-        print "Features DataFrame %s" % type(features_df)
-
-        print("--------------------")
-        print("--------------------")
-        print("--------------------")
-        print(type(features_df)
-        print("--------------------")
-        print("--------------------")
-        print("--------------------")
-        
-        
-        
+    def predict(self, features_df):       
+        # Load model from file
         folder_path = self.data_folder
         pkl_filename = folder_path + "/pickle_model.pkl"
         
-        # Load from file
         with open(pkl_filename, 'rb') as file:
             pickle_model = pickle.load(file)
 
         
-        
-        # predictions, one per record (features_df row)
+        # Get predictions
         #predictions = pd.Series(["good", "fair", "poor", "good", "poor"])
         predictions = pd.Series(pickle_model.predict(features_df))
         
-        # optional probas for each class (may be None or a DataFrame with one column per class)
+        
+        # Get probas (optional)
         #probas = pd.DataFrame({
         #    'proba_good': pd.Series([.9, .6, .2, .7, .2]),
         #    'proba_fair': pd.Series([.2, .7, .3, .3, .3]),
