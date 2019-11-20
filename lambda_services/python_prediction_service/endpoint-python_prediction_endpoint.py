0bbe58b15c0946fa468d73903a5502170d65935f
admin <admin>
2019-11-20 11:49:49 +0100

    Updated Lambda service config

--- lambda_services/python_prediction_service/endpoint-python_prediction_endpoint.py
+++ lambda_services/python_prediction_service/endpoint-python_prediction_endpoint.py
@@ -8,7 +8,6 @@ class MyPredictor(ClassificationPredictor):
         """data_folder is the absolute path to the managed folder storing the data for the model
         (if any)"""
         self.data_folder = data_folder
-        print data_folder
 
     def predict(self, features_df):
         """
@@ -46,15 +45,29 @@ class MyPredictor(ClassificationPredictor):
         print("--------------------")
         print("--------------------")
         print("--------------------")
-        # predictions, one per record (features_df row)
-        predictions = pd.Series(["good", "fair", "poor", "good", "poor"])
-
-        # optional probas for each class (may be None or a DataFrame with one column per class)
-        probas = pd.DataFrame({
-            'proba_good': pd.Series([.9, .6, .2, .7, .2]),
-            'proba_fair': pd.Series([.2, .7, .3, .3, .3]),
-            'proba_poor': pd.Series([.2, .6, .6, .3, .9])
-        })
         
+        
+        
+        import pickle
+        folder_path = self.data_folder
+        pkl_filename = folder_path + "/pickle_model.pkl"
+        
+        # Load from file
+        with open(pkl_filename, 'rb') as file:
+            pickle_model = pickle.load(file)
 
-        return (predictions, probas)
+        
+        
+        # predictions, one per record (features_df row)
+        #predictions = pd.Series(["good", "fair", "poor", "good", "poor"])
+        predictions = pickle_model.predict(features_df)
+        
+        # optional probas for each class (may be None or a DataFrame with one column per class)
+        #probas = pd.DataFrame({
+        #    'proba_good': pd.Series([.9, .6, .2, .7, .2]),
+        #    'proba_fair': pd.Series([.2, .7, .3, .3, .3]),
+        #    'proba_poor': pd.Series([.2, .6, .6, .3, .9])
+        #})
+        probas = 
+
+        return (predictions) #, probas
