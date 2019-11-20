bc447cb6b25815a596b251efb73a59c209a777b4
admin <admin>
2019-11-20 12:06:26 +0100

    Updated Lambda service config

--- lambda_services/python_prediction_service/endpoint-python_prediction_endpoint.py
+++ lambda_services/python_prediction_service/endpoint-python_prediction_endpoint.py
@@ -4,46 +4,19 @@ import pandas as pd
 import pickle
 
 class MyPredictor(ClassificationPredictor):
-    """The class for a classification Custom API node predictor"""
 
     def __init__(self, data_folder):
-        """data_folder is the absolute path to the managed folder storing the data for the model
-        (if any)"""
         self.data_folder = data_folder
 
     def predict(self, features_df):
-        """
-        The main prediction method.
 
-        :param: df: a dataframe of 1 or several records to predict
 
-        :return: Either:
-            ``decision_series`` or
-            ``(decision_series, proba_df)`` or
-            ``(decision_series, proba_df, custom_keys_list)``
-
-        decision_series must be a Pandas Series of decisions
-
-        proba_df is optional and must contain one column per class
-
-        custom_keys_list is optional and must contain one entry per input row. Each entry of
-        custom_keys_list must be a Python dictionary. These custom keys will be sent in the
-        output result
-
-        decision_series, proba_df and custom_keys_list must have the same number of rows than df.
-        """
-
-        # Note: this sample uses the second form (decision_series, proba_df)
-
-        # Note: this sample "cheats" and always returns 5 predictions.
-        # You should actually return 1 prediction per row in the features_df
-
-        print "Features DataFrame %s" % features_df
+        print "Features DataFrame %s" % type(features_df)
 
         print("--------------------")
         print("--------------------")
         print("--------------------")
-        print(self.data_folder)
+        print(features_df)
         print("--------------------")
         print("--------------------")
         print("--------------------")
